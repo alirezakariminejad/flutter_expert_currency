9dad4f455c16f8f09c443853e6a30cd9595d35cb
Alireza kariminejad <alireza.kariminejad@yahoo.com>
2023-06-08 20:32:57 +0330

    Add theme_switcher_helper

---
+++ lib/screens/screens_helper/theme_switcher_helper.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ThemeSwitcherHelper extends StatelessWidget {
+  const ThemeSwitcherHelper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {},
+      icon: Icon(Icons.sunny),
+    );
+  }
+}
